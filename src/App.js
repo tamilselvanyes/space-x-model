b4b201c758505584ccba547895b55b49de69ed2b
Tamil Selvan S <tamilselvanyes@gmail.com>
2022-05-19 22:51:20 +0530

    Footer design

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
-import logo from "./logo.svg";
 import "./App.css";
+import "./css/reset.css";
+import "./css/footer.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <p>SpaceX</p>
+      <FooterOptions />
     </div>
   );
 }
 
-export default App;
+function FooterOptions() {
+  return (
+    <footer className="footer">
+      <ul className="footer_list">
+        <li>
+          <a class="footer_list_item" href="#">
+            SpaceX © 2022
+          </a>
+        </li>
+        <li>
+          <a class="footer_list_item" href="#">
+            Twitter
+          </a>
+        </li>
+        <li>
+          <a class="footer_list_item" href="#">
+            Youtube
+          </a>
+        </li>
+        <li>
+          <a class="footer_list_item" href="#">
+            Instagram
+          </a>
+        </li>
+        <li>
+          <a class="footer_list_item" href="#">
+            Linkedin
+          </a>
+        </li>
+        <li>
+          <a class="footer_list_item" href="#">
+            Privacy Policy
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
+}
